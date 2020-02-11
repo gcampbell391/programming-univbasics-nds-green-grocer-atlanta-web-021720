013fa426e25c668d7a98542015e85f9d39fbd143
Gene Campbell III <gcampbell391@gmail.com>
2020-02-11 00:42:34 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -60,9 +60,11 @@ cart
 end 
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  counter = 0 
+  while counter < cart.length 
+    if cart[counter]
+  counter += 1 
+end 
 end
 
 def checkout(cart, coupons)
