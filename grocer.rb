505e968ac4eb620c37f43a9dda8015cbdc5709d7
Gene Campbell III <gcampbell391@gmail.com>
2020-02-11 01:03:05 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -64,13 +64,13 @@ def apply_clearance(cart)
   while counter < cart.length do
     if cart[counter][:clearance] == true 
       item_price = cart[counter][:price]
-     pp new_item_price = item_price - (item_price* 0.2 )
-      item_price = new_item_price
+     new_item_price = item_price - (item_price* 0.2 )
+      cart[counter][:price] = new_item_price
     else
     end 
     counter += 1
 end 
-cart
+pp cart
 end
 
 def checkout(cart, coupons)
